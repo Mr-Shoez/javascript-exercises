76c457fa10a00e74e9d657e91fbdd15c75d6a373
Mr-Shoez <molelekishoez@gmail.com>
2026-02-11 18:02:14 +0200

    had to work for this one, but i got it, i am really learning how the code in javascript is read.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, integer) {
+ const theWord = string;
+ const theNumber = integer;
 
+
+ if (theNumber < 0) {
+    return "ERROR";
+  }
+ let dupWord =  "";
+ 
+
+ for (let i = 0; i < theNumber; i++) {
+    dupWord += theWord;
+    
+ }
+ return(dupWord);
 };
+
+console.log(repeatString("mosa", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
